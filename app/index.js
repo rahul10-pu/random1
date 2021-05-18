8670cdf4d1b04de619db953c823e201d7139bbc6
Rahul Kumar <rahul10.pu@gmail.com>
2021-05-18 22:26:38 +0530

    just deleted my codes

--- app/index.js
+++
@@ -1,7 +0,0 @@
-const http = require('http')
-const server = http.createServer((req,res) => {
-    console.log(req)
-    res.write("hello")
-    res.end()
-})
-server.listen(8080,()=> console.log('Server is running'))
--- demoapp/server.js
+++
@@ -1,7 +0,0 @@
-const hostname='127.0.0.1'
-const port=8000;
-
-const server = require('./controller/controller')
-server.listen(port, hostname,()=>{
-    console.log(`server running at http://${hostname}:${port}/`)
-});
