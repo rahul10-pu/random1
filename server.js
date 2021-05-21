a7c9ea03e3d49d9677a8157f1f6f1115e865b609
Govind <vermsgovnd@gmail.com>
2021-05-22 02:41:29 +0530

    server file  deleted

--- server.js
+++
@@ -1,9 +0,0 @@
-const { isMaster } = require('cluster')
-const http = require('http')
-const server = http.createServer((req,res) => {
-    console.log(req)
-    res.write("hello")
-    res.end()
-})
-server.listen(8080,()=> console.log('Server is running'))
-// my name is Govind
